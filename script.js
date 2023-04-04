25f664c057309d4217c356c5c3be83c6e7c1f8e5
peotr26 <67782057+peotr26@users.noreply.github.com>
2023-04-04 18:41:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-console.log(matched);
-
 let body = document.querySelector("body");
 let content = document.getElementById("content");
 
-if (dark) {
-	body.style.backgroundColor = "#1a1a1a";
-	content.setAttribute("class", "dark");
-} else {
-	body.style.backgroundColor = "#f5f5f5";
-	content.setAttribute("class", "light");
-}
+window
+	.matchMedia("(prefers-color-scheme: dark)")
+	.addEventListener("change", function (e) {
+		const colorScheme = e.matches ? "dark" : "light";
+		console.log(colorScheme);
+
+		if (colorScheme === "dark") {
+			body.style.backgroundColor = "#1a1a1a";
+			content.setAttribute("class", "dark");
+		} else {
+			body.style.backgroundColor = "#f5f5f5";
+			content.setAttribute("class", "light");
+		}
+	});
